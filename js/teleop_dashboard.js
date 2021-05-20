43936056cab28ec64dc6f35c04cdd4c252865f92
Yash Vakil <yashvakil5@gmail.com>
2021-05-20 13:14:50 -0400

    Initial Commit

---
+++ js/teleop_dashboard.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yrvak on 19-05-2021.
+ */
+function getAccess(robotId) {
+    var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function () {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         document.getElementById("demo").innerHTML = xhttp.responseText;
+    //     }
+    // };
+    // xhttp.open("GET", "filename", true);
+    // xhttp.send();
+}
